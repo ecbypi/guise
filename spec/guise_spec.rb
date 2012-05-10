386712c5c0995ac5a98e14e88c464904de36bdaa
Eduardo Gutierrez <edd_d@mit.edu>
2012-05-10 19:25:53 -0400

    Collapse specs into one file
    Project isn't big or complicated enough to warrant splitting up
    functionality for the different modules into their own tests.

---
+++ spec/guise_spec.rb
@@ -0,0 +1,55 @@
+require 'spec_helper'
+
+describe Guise do
+
+  let(:user) { create(:user) }
+  let(:supervisor) { create(:supervisor) }
+  let(:technician) { create(:technician) }
+
+  it "builds subclasses of names called in :guise" do
+    Technician.new.should be_a User
+    Technician.new.guises.should_not be_empty
+  end
+
+  it "adds scopes for each type" do
+    User.technicians.should include(technician)
+    User.technicians.should_not include(user)
+
+    User.supervisors.should include(supervisor)
+    User.supervisors.should_not include(user)
+  end
+
+  describe "#has_role?" do
+    it "checks if resource is of the type provided" do
+      user.has_role?(:technician).should be_false
+      technician.has_role?(:Technician).should be_true
+    end
+
+    it "raises an error if type was not added in :guises call" do
+      expect { user.has_role?(:Accountant) }.to raise_error(NameError)
+    end
+  end
+
+  describe "#has_roles?" do
+    before :each do
+      create(:user_role, :name => 'Technician', :user => supervisor)
+    end
+
+    it "checks if resource is all of the provided types" do
+      technician.has_roles?(:Supervisor, :Technician).should be_false
+      supervisor.has_roles?('Supervisor', Technician).should be_true
+    end
+  end
+
+  describe "#has_any_roles?" do
+    it "checks if resource is any of the supplied roles" do
+      user.has_any_roles?(:Supervisor, :Technician).should be_false
+      technician.has_any_roles?('supervisor', 'technician').should be_true
+    end
+  end
+
+  it "adds methods that proxy to #has_role? for ease" do
+    user.should respond_to :technician?
+    user.should respond_to :supervisor?
+  end
+end
