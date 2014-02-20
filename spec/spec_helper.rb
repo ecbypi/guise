30b15adb8e2b25361c1dfb5cff9a82feb1b3e719
Eduardo Gutierrez <eduardo@vermonster.com>
2014-02-19 20:55:30 -0500

    Remove `spec/support/database.rb`

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,15 +3,39 @@ require 'factory_girl'
 require 'shoulda-matchers'
 require 'guise'
 
-require File.expand_path('../support/database.rb', __FILE__)
+ActiveRecord::Base.establish_connection(
+  :adapter => 'sqlite3',
+  :database => ':memory:'
+)
 
-RSpec.configure do |config|
+ActiveRecord::Migration.verbose = false
 
-  config.before :suite do
-    Database.create
-    FactoryGirl.find_definitions
+ActiveRecord::Schema.define do
+  create_table :users, :force => true do |t|
+    t.string :name
+    t.string :email
   end
 
-  config.include FactoryGirl::Syntax::Methods
+  create_table :user_roles, :force => true do |t|
+    t.string :name
+    t.integer :person_id
+  end
 end
 
+class User < ActiveRecord::Base
+  has_guises :Technician, :Supervisor,
+             :association => :user_roles,
+             :attribute => :name,
+             :foreign_key => :person_id
+end
+
+class UserRole < ActiveRecord::Base
+  guise_for :user,
+            :foreign_key => :person_id
+end
+
+
+FactoryGirl.find_definitions
+RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+end
